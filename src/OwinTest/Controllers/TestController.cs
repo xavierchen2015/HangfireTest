ac4c9e564d11fa9f59b2b779532a645dc5ace904
XavierChen2015 <XavierChen2015@gmail.com>
2015-10-05 13:36:08 +0800

    new project owintest

---
+++ src/OwinTest/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OwinTest.Controllers
+{
+    public class Fromv
+    {
+        public string value { get; set; }
+    }
+    public class TestController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public string Post(Fromv Fvalue)
+        {
+            return Fvalue.value;
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
